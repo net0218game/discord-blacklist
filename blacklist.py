4acdb1b6fe58078147f7fd09bb65f50eec8224e2
David <58487627+net0218game@users.noreply.github.com>
2021-01-27 09:52:37 +0100

    Update blacklist.py

--- blacklist.py
+++ blacklist.py
@@ -1,43 +1,39 @@
+from discord.ext import commands, tasks
 import discord
-import asyncio
-from discord.ext import commands
+from datetime import datetime
+import random
 
 client = commands.Bot(command_prefix=".")
 
+status = ["Nem jacok semmit hagyal", "Roblox", "Gebedek", "Ezt most komolyan van aki elolvassa?",
+            "En csak egy csicska vagyok, szerinted mit jacok?", "Fortnajtozok"]
+
+words = ["lol", "k", "ok"]
+
 @client.event
 async def on_ready():
+    change_status.start()
     await client.change_presence(status=discord.Status.online)
-    print("msgrec bot is ready!")
-    file1 = open("messages.txt", "a")
-    file1.write(f"-------------------------------------------------------------------\n")
-    file1.close()
+    print(">>> [" + datetime.now().strftime("%H:%M:%S") + "]", "BOT IS RUNNING")
+
+@tasks.loop(seconds=150)
+async def change_status():
+    await client.change_presence(activity=discord.Game(random.choice(status)))
+    print(">>> [" + datetime.now().strftime("%H:%M:%S") + "]", "Status changed!")
+
+@client.command()
+async def ping(message):
+    await message.send(f"*Pong!* My ping is: {round(client.latency * 1000)} ms")
+    print(">>> [" + datetime.now().strftime("%H:%M:%S") + "]", "Ping!")
 
 @client.event
 async def on_message(message):
-    users_id = message.author.id
-    channel = message.channel
-    time = message.created_at
     server = message.guild
-    attachments = message.attachments
-    fetchMessage = await channel.history().find(lambda m: m.author.id == users_id)
-    text = (f"----------{message.author}----------\nüzenet: {fetchMessage.content}\nserver: {server}\ncsatolt file-ok: {attachments}\ndátum: {time}\n========================================")
-    file1 = open("messages.txt", "a", encoding="utf-8")
-    file1.write(f"{text} \n")
-    file1.close()
+    text = str(message.content).strip().split()
+    for i in range(len(words)):
+        word = str(words[i])
+        if word in text:
+            print(f">>> [" + datetime.now().strftime("%H:%M:%S") + "]", f"Blocked word in text!\nServer: {server.id}\nMessage: {message.content}")
+            await message.delete()
 
-@client.event
-async def on_message_delete(message):
-    users_id = message.author.id
-    channel = message.channel
-    time = message.created_at
-    server = message.guild
-    attachments = message.attachments
-    fetchMessage = await channel.history().find(lambda m: m.author.id == users_id)
-    text = (f"----------[{message.author} DELETED A MESSAGE]----------\ntörölt üzenet: {fetchMessage.content}\nserver: {server}\ncsatolt file-ok: {attachments}\ndátum: {time}\n========================================")
-    print(text)
-    file1 = open("messages.txt", "a", encoding="utf-8")
-    file1.write(f"{text} \n")
-    file1.close()
-
-
-client.run(TOKEN GOES HERE)
+client.run("TOKEN GOES HERE")
