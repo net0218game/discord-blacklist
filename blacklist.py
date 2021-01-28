a7850167d4fe912c1de9d2435455919b3ba71471
David <58487627+net0218game@users.noreply.github.com>
2021-01-28 16:40:41 +0100

    Update blacklist.py

--- blacklist.py
+++ blacklist.py
@@ -8,7 +8,12 @@ client = commands.Bot(command_prefix=".")
 status = ["Nem jacok semmit hagyal", "Roblox", "Gebedek", "Ezt most komolyan van aki elolvassa?",
             "En csak egy csicska vagyok, szerinted mit jacok?", "Fortnajtozok"]
 
-words = ["lol", "k", "ok"]
+blacklist = open("blacklist.txt", "r", encoding="utf-8")
+words = []
+for line in blacklist:
+    words.append(line.strip())
+blacklist.close()
+mode = True
 
 @client.event
 async def on_ready():
@@ -16,10 +21,53 @@ async def on_ready():
     await client.change_presence(status=discord.Status.online)
     print(">>> [" + datetime.now().strftime("%H:%M:%S") + "]", "BOT IS RUNNING")
 
-@tasks.loop(seconds=150)
+@tasks.loop(seconds=1800)
 async def change_status():
     await client.change_presence(activity=discord.Game(random.choice(status)))
     print(">>> [" + datetime.now().strftime("%H:%M:%S") + "]", "Status changed!")
+
+@commands.has_permissions(administrator=True)
+@client.command()
+async def off(message):
+    server = message.guild
+    global mode
+    mode = False
+    await message.send("Blacklist tuned off. Type '.off' to turn it on.")
+    print(f">>> [" + datetime.now().strftime("%H:%M:%S") + "]",
+          f"Blacklist has been turned off at:\nServer: {server}")
+
+@commands.has_permissions(administrator=True)
+@client.command()
+async def on(message):
+    server = message.guild
+    global mode
+    mode = True
+    await message.send("Blacklist tuned on. Type '.off' to turn it off.")
+    print(f">>> [" + datetime.now().strftime("%H:%M:%S") + "]",
+          f"Blacklist has been turned on at:\nServer: {server}")
+
+@commands.has_permissions(administrator=True)
+@client.command()
+async def add(message, word):
+    server = message.guild
+    blacklist = open("blacklist.txt", "a")
+    word = str(word).lower()
+    blacklist.write(f"\n{word}")
+    print(f">>> [" + datetime.now().strftime("%H:%M:%S") + "]",
+          f"Blacklist appended with {word} at:\nServer: {server}")
+
+@client.event
+async def on_message(message):
+    server = message.guild
+    text = str(message.content).lower().strip().split()
+    if (mode == True):
+        for i in range(len(words)):
+            word = str(words[i])
+            if word in text:
+                print(f">>> [" + datetime.now().strftime("%H:%M:%S") + "]",
+                      f"Blocked word in text!\nServer: {server}\nMessage: {message.content}")
+                await message.delete()
+    await client.process_commands(message)
 
 @client.command()
 async def ping(message):
@@ -27,13 +75,12 @@ async def ping(message):
     print(">>> [" + datetime.now().strftime("%H:%M:%S") + "]", "Ping!")
 
 @client.event
-async def on_message(message):
-    server = message.guild
-    text = str(message.content).strip().split()
-    for i in range(len(words)):
-        word = str(words[i])
-        if word in text:
-            print(f">>> [" + datetime.now().strftime("%H:%M:%S") + "]", f"Blocked word in text!\nServer: {server.id}\nMessage: {message.content}")
-            await message.delete()
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandNotFound):
+        await ctx.send("Invalid command!")
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send("Please fill in all requirements.")
+    if isinstance(error, commands.MissingPermissions):
+        await ctx.send("You dont have permissions for using this command.")
 
 client.run("TOKEN GOES HERE")
